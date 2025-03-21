5131515d38fc30031ce1bea153ba65f9b86a6000
Hannah <hsistrunk@gmail.com>
2025-03-21 12:16:03 -0400

    add styles and basic page structure

---
+++ app/errors/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Package Errors - zodiac",
+};
+
+export default function Errors() {
+  return (
+    <div>
+      <h1>Package Errors</h1>
+      {/* TODO: Insert table with columns for:
+      - package title (as link to packages/:id page)
+      - package id
+      - Service error (Error - application:service as link to error events/:id page)
+      - Date/time */}
+    </div>
+  );
+}

da2e3c2a992cf968930d35edec0656d6beffcba1
Hannah <hsistrunk@gmail.com>
2025-03-23 14:20:33 -0400

    improve fetch error handling

--- app/lib/fetchData.js
+++ app/lib/fetchData.js
@@ -1,10 +1,20 @@
 // Return the `results` array from a specified API endpoint
 export async function fetchData(apiEndpoint) {
-  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL; //specified in .env file
-  const res = await fetch(`${baseURL}${apiEndpoint}`);
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL; // specified in .env file
 
-  if (!res.ok) throw new Error(`Failed to fetch data: ${res.status}`);
-  const data = await res.json();
+  try {
+    const res = await fetch(`${baseURL}${apiEndpoint}`);
 
-  return data.results;
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data from ${apiEndpoint}. Status: ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    return data.results || [];
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    // Create error for a UI component to display
+    throw new Error(`Error fetching data from ${apiEndpoint}: ${error.message}`);
+  }
 }

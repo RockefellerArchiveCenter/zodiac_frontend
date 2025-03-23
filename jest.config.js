8baede2916dd5e67216a9c95f85b621755c04d28
Hannah <hsistrunk@gmail.com>
2025-03-23 15:50:30 -0400

    set up test structure

--- jest.config.js
+++ jest.config.js
@@ -129,7 +129,7 @@ const config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+  rootDir: './app',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -172,7 +172,7 @@ const config = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
+  testRegex: '.*\\.test\\.js$',
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,

9fba52fdf43511832b3bbec2dad3c14b50edddde
Hillel Arnold <hillel.arnold@gmail.com>
2025-03-27 11:09:55 -0400

    formatting

--- app/__tests__/Button.test.js
+++ app/__tests__/Button.test.js
@@ -7,28 +7,28 @@ describe('Button Component', () => {
   it('renders the button', () => {
 
     render(
-    <Button 
-        type='submit'
-        color='blue'
-        size='md'
-        handleClick={jest.fn()}
-        ariaLabel='Submit form'
-        ariaHasPopup={false}
-        ariaExpanded={true}
-        ariaPressed={true}
-        disabled={false}
-        label='Submit form'
-         />);
+      <Button 
+          type='submit'
+          color='blue'
+          size='md'
+          handleClick={jest.fn()}
+          ariaLabel='Submit form'
+          ariaHasPopup={false}
+          ariaExpanded={true}
+          ariaPressed={true}
+          disabled={false}
+          label='Submit form'
+          />);
     
-        const button = screen.getByText('Submit form')
-        expect(button).toBeInTheDocument()
-        expect(button.className).toBe('btn btn--blue btn--md')
-        expect(button.type).toBe('submit')
-        expect(button.getAttribute('aria-label')).toBe('Submit form')
-        expect(button.getAttribute('aria-haspopup')).toBe('false')
-        expect(button.getAttribute('aria-expanded')).toBe('true')
-        expect(button.getAttribute('aria-pressed')).toBe('true')
-        expect(button.disabled).toBeFalsy()
+      const button = screen.getByText('Submit form')
+      expect(button).toBeInTheDocument()
+      expect(button.className).toBe('btn btn--blue btn--md')
+      expect(button.type).toBe('submit')
+      expect(button.getAttribute('aria-label')).toBe('Submit form')
+      expect(button.getAttribute('aria-haspopup')).toBe('false')
+      expect(button.getAttribute('aria-expanded')).toBe('true')
+      expect(button.getAttribute('aria-pressed')).toBe('true')
+      expect(button.disabled).toBeFalsy()
     
   });
 

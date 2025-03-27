4ceca0dde3d38c1d2a544969b1ed7a9b186a746f
Hillel Arnold <hillel.arnold@gmail.com>
2025-03-27 11:27:49 -0400

    remove proptypes

--- app/components/SummaryList.js
+++ app/components/SummaryList.js
@@ -1,16 +1,9 @@
-import PropTypes from 'prop-types'
-
 export const SummaryListItem = ({label, value}) => (
     <div className="summary-list__row">
         <dt className="summary-list__key">{label}</dt>
         <dd className="summary-list__value">{value}</dd>
     </div>
 )
-
-SummaryListItem.propTypes = {
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired
-}
 
 export const SummaryList = ({ title, children }) => (
     <div className="card">
@@ -20,8 +13,3 @@ export const SummaryList = ({ title, children }) => (
         </dl>
     </div>
 )
-
-SummaryList.propTypes = {
-    title: PropTypes.string,
-    children: PropTypes.arrayOf(PropTypes.element)
-}

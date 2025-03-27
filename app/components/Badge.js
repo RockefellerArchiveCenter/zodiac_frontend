d37d5a69116cd667705a758bb43fdd7a20bd747b
Hillel Arnold <hillel.arnold@gmail.com>
2025-03-27 11:15:32 -0400

    remove proptypes

--- app/components/Badge.js
+++ app/components/Badge.js
@@ -1,14 +1,7 @@
-import PropTypes from 'prop-types'
-
 const Badge = ({ color, text }) => (
   <span className={`badge badge--${color}`}>
     {text}
   </span>
 )
-
-Badge.propTypes = {
-  color: PropTypes.oneOf(['orange', 'blue', 'light-blue']).isRequired,
-  text: PropTypes.string.isRequired
-}
 
 export default Badge;

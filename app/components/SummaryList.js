569a7f5d1fea9e545cf431f30da69db0d566af70
Hillel Arnold <hillel.arnold@gmail.com>
2025-03-27 10:33:35 -0400

    add PropTypes

--- app/components/SummaryList.js
+++ app/components/SummaryList.js
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types'
+
 export const SummaryListItem = ({label, value}) => (
     <div className="summary-list__row">
         <dt className="summary-list__key">{label}</dt>
         <dd className="summary-list__value">{value}</dd>
     </div>
 )
+
+SummaryListItem.propTypes = {
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired
+}
 
 export const SummaryList = ({ title, children }) => (
     <div className="card">
@@ -13,3 +20,8 @@ export const SummaryList = ({ title, children }) => (
         </dl>
     </div>
 )
+
+SummaryList.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.arrayOf(PropTypes.element)
+}

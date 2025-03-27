15e682398934bb331a2d458305a46c5f2e3d9249
Hillel Arnold <hillel.arnold@gmail.com>
2025-03-27 11:10:13 -0400

    remove PropTypes

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types'
-
 const Button = props => (
   <button
     type={props.type}
@@ -9,29 +7,8 @@ const Button = props => (
     aria-haspopup={props.ariaHasPopup}
     aria-expanded={props.ariaExpanded}
     aria-pressed={props.ariaPressed}
-    disabled={props.disabled} >
+    disabled={props.disabled}>
       {props.label}
   </button>)
-
-Button.propTypes = {
-  type: PropTypes.oneOf(['button', 'submit', 'reset']).isRequired,
-  color: PropTypes.oneOf([
-    'light-blue', 
-    'blue', 
-    'navy', 
-    'orange', 
-    'transparent', 
-    'gray', 
-    'dark-gray', 
-    'black']).isRequired,
-  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg']).isRequired,
-  handleClick: PropTypes.func.isRequired,
-  ariaLabel: PropTypes.string,
-  ariaHasPopup: PropTypes.string,
-  ariaExpanded: PropTypes.string,
-  ariaPressed: PropTypes.string,
-  disabled: PropTypes.bool,
-  label: PropTypes.string.isRequired
-}
 
 export default Button

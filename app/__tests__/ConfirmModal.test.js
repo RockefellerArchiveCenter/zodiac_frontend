3ce633ee95b4ce0e9c0718072136a84e88d6740b
Hannah <hsistrunk@gmail.com>
2025-03-28 15:19:58 -0400

    linting

--- app/__tests__/ConfirmModal.test.js
+++ app/__tests__/ConfirmModal.test.js
@@ -1,25 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import ConfirmModal from '../components/ConfirmModal';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ConfirmModal from "../components/ConfirmModal";
 
+describe("Confirm Modal Component", () => {
+  it("renders the modal", () => {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
 
-describe('Confirm Modal Component', () => {
+    render(
+      <ConfirmModal
+        appElement={container}
+        className="modal--confirm"
+        isOpen={true}
+        title="Confirm request"
+        content="Are you sure you want to trigger this service?"
+      />,
+    );
 
-  it('renders the modal', () => {
-
-    const container = document.createElement('div')
-    document.body.appendChild(container)
-
-    render(<ConfirmModal
-      appElement={container}
-      className='modal--confirm' 
-      isOpen={true}
-      title='Confirm request'
-      content='Are you sure you want to trigger this service?' />);
-
-    expect(screen.getByText('Confirm request')).toBeInTheDocument();
-    expect(screen.getByText('Are you sure you want to trigger this service?')).toBeInTheDocument();
-    expect(screen.getByText('Confirm request').parentElement.parentElement).toHaveClass('modal--confirm');
+    expect(screen.getByText("Confirm request")).toBeInTheDocument();
+    expect(
+      screen.getByText("Are you sure you want to trigger this service?"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Confirm request").parentElement.parentElement,
+    ).toHaveClass("modal--confirm");
   });
-
 });
--- app/components/ConfirmModal.js
+++ app/components/ConfirmModal.js
@@ -1,22 +1,34 @@
-import Modal from 'react-modal'
+import Modal from "react-modal";
 
-const ConfirmModal = ({ appElement='#root', className, isOpen, toggleModal, title, content }) => (
+const ConfirmModal = ({
+  appElement = "#root",
+  className,
+  isOpen,
+  toggleModal,
+  title,
+  content,
+}) => (
   <Modal
-    appElement={appElement ? appElement : Modal.setAppElement('#root')}
+    appElement={appElement ? appElement : Modal.setAppElement("#root")}
     isOpen={isOpen}
     onRequestClose={toggleModal}
     className={`modal ${className}`}
-    overlayClassName='modal__overlay'>
-    <div className='modal__header'>
-      <h2 className='modal__header-title'>{title}</h2>
-      <button className='modal__header-button' aria-label='Close' onClick={toggleModal}>
-        <span className='modal__icon' aria-hidden="true">icon_close</span>
+    overlayClassName="modal__overlay"
+  >
+    <div className="modal__header">
+      <h2 className="modal__header-title">{title}</h2>
+      <button
+        className="modal__header-button"
+        aria-label="Close"
+        onClick={toggleModal}
+      >
+        <span className="modal__icon" aria-hidden="true">
+          icon_close
+        </span>
       </button>
     </div>
-    <div className='modal__body'>
-      {content}
-    </div>
+    <div className="modal__body">{content}</div>
   </Modal>
-)
+);
 
-export default ConfirmModal
+export default ConfirmModal;
